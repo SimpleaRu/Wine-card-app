0f4dbb3fdf93e757bb5373cc564161f355934d7a
Кашуба Александр Александрович <a-kashuba@AG.local>
2017-12-20 17:29:09 +0300

    12.20

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ var wineCards = [
         originText: 'Крым, Массандра',
         priceText: '300&#8381;',
         noteText: 'Молодое крымское вино'
-    }
+    },
+    {
+        name: 'Cabernet Saperavi',
+        type: 'dry',
+        colorType: 'red',
+        imgUrl: '../img/cabernet_saperavi.jpg',
+        colorText: 'Пурпурный',
+        aromeText: 'Ноты чёрной смородины',
+        tasteText: 'Сбалансированный, немного танинный',
+        originText: 'Крым, Массандра',
+        priceText: '300&#8381;',
+        noteText: 'Молодое крымское вино'
+    },
+    {
+        name: 'Cabernet',
+        type: 'dry',
+        colorType: 'red',
+        imgUrl: '',
+        colorText: 'Пурпурный',
+        aromeText: 'Ноты чёрной смородины',
+        tasteText: 'Сбалансированный, немного танинный',
+        originText: 'Крым, Массандра',
+        priceText: '300&#8381;',
+        noteText: 'Молодое крымское вино'
+    },
+    {
+        name: 'Saperavi',
+        type: 'dry',
+        colorType: 'red',
+        imgUrl: '',
+        colorText: 'Пурпурный',
+        aromeText: 'Ноты чёрной смородины',
+        tasteText: 'Сбалансированный, немного танинный',
+        originText: 'Крым, Массандра',
+        priceText: '300&#8381;',
+        noteText: 'Молодое крымское вино'
+    },
 ];
 
 let template = render({ list: wineCards });
