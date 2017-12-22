f7cb89a9b569aeb092a2d0df993ff310b5d0c2c2
Кашуба Александр Александрович <a-kashuba@AG.local>
2017-12-22 13:55:59 +0300

    php_0.1

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
 	
 		$string = file_get_contents('winecardsJSON.json');
 		$data = json_decode($string);
-
 		var_dump($data[1]->name);
+		$data[1]->noteText = 'trololo';
 		echo $data[1]->noteText;
 		
 	?>
