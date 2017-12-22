02f2daa5cc8a0593aec93d973e4fe6840b59aae2
Кашуба Александр Александрович <a-kashuba@AG.local>
2017-12-22 17:29:25 +0300

    php_0.2

--- index.php
+++ index.php
@@ -2,23 +2,23 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-
 	<title>PHP_n_JSON</title>
+	
 </head>
 <body>
-	<p>
-		some text
-	</p>
+	<div class="wineContener"></div>
+	
 	<?php
 	
-		$string = file_get_contents('winecardsJSON.json');
-		$data = json_decode($string);
-		var_dump($data[1]->name);
-		$data[1]->noteText = 'trololo';
-		echo $data[1]->noteText;
-		
+	$string = file_get_contents('winecardsJSON.json');
+	$data = json_decode($string);
+	var_dump($data[1]->name);
+	$data[1]->noteText = 'trololo';
+	echo $data[1]->noteText;
+	
 	?>
-	<script src="script.js"></script>
+
+	<script src='script.js'></script>
 </body>
 </html>
 
--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-var container = document.querySelector('body');
+var wineContener = document.querySelector('.wineContener');
+console.log(wineContener);
+fetch('winecardsJSON.json')
+.then(function (result){
+    return result.json();
+})
+.then (function (result){
+    console.log(result);
+})
+.catch(alert);
+
+var img1 = document.createElement('img');
+img1.setAttribute('src', 'img/1.png');
+console.log(img1);
+wineContener.appendChild(img1);
