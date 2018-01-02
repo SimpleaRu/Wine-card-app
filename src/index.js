6ca9e9a7b99f62f223763dfbf69934fcefd35684
SimpleaRU <info@simplea.ru>
2018-01-02 11:38:12 +0300

    compound_JS_PHP

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ var wineWraper = document.querySelector('.wine-cards');
 var addCardButton = document.querySelector('.add-button');
 var addWineForm = document.querySelector('.add-wine-form-wrapper');
 var formClose = document.querySelector('#formClose');
+var form = document.forms.namedItem('addcard');
+var fileFild = document.querySelector('#fileFild');
+var wineCardsArr;
 
 function IsJsonString(str) {
     try {
@@ -18,7 +21,7 @@ function IsJsonString(str) {
 function renderCards() {
 
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', '../src/winecardsJSON.json', true);
+    xhr.open('GET', '../src/winecardsJSON.json', true); // winecardsJSON.json
 
     xhr.send();
     xhr.addEventListener('load', function () {
@@ -48,27 +51,45 @@ addCardButton.addEventListener('click', function (e) {
     addWineForm.style.display = 'block';
 
 });
-formClose.addEventListener('click', function (e) {
+/* formClose.addEventListener('click', function (e) {
     addWineForm.style.display = 'none';
-});
+}); */
 
 document.body.addEventListener('click', function (e) {
-    if (e.target == addWineForm) {
+    if (e.target == addWineForm || e.target == formClose) {
         addWineForm.style.display = 'none';
     }
 });
 
-/* fetch('../src/winecardsJSON.json') // '../src/winecardsJSON.json' 
-    // http://simplea.ru/wineCards/winecardsJSON.json
-    .then(function (resp) {
-    // console.log( JSON.parse(resp));
-        return resp.json(); // resp.json()
-    })
-    .then(function (j) {
+form.addEventListener('submit', function (e) {
+    if (fileFild.files[0].size > 400000) {
+        console.log('file too big');
+        e.preventDefault();
+    }
+    else {
+        var oData = new FormData(form);
+        var oReq = new XMLHttpRequest();
+        oReq.open("POST", "functions.php", true);
+        oReq.onload = function (oEvent) {
+    
+            if (oReq.status == 200) {
+                console.log("Uploaded!");
+                renderCards();
+    
+                //  console.log(oReq.response);
+                //    var jsonResp = JSON.parse(oReq.response);
+                //     console.log(oReq.response); 
+            } else {
+                console.warn("Error " + oReq.status + " occurred when trying to upload your file");
+            }
+        };
+    
+        oReq.send(oData); 
+        e.preventDefault();   
+    }
+});
 
-     //  console.log(j[0].imgUrl);
-        let template = render({ list: j });
+fileFild.addEventListener('change', function(e) {
+console.log(fileFild.files[0].size);
 
-        wineWraper.innerHTML = template;
-    })
-    .catch(alert); */
+});
